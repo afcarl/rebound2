2be7e17b2fb1c8049ce0a1a7601719bef7155f28
silburt <silburt@astro.utoronto.ca>
2015-10-27 13:09:39 -0400

    Oct27 xyz_output.py

---
+++ examples/planetesimals2/xyz_output.py
@@ -0,0 +1,35 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import glob
+import matplotlib.cm as cm
+pi = math.pi
+from mpl_toolkits.mplot3d import Axes3D
+import re
+
+def natural_key(string_):
+    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
+
+file_names = 'xyz_temp/outputmini_'
+files = glob.glob(file_names+'*.txt')
+files = sorted(files, key=natural_key)
+N_files = len(files)  #number of files we're dealing with
+N_planets = 500
+
+#colors
+colors =  ["black" for x in range(N_planets)]
+colors[0] = 'yellow'
+colors[1] = 'orange'
+colors[2] = 'red'
+
+for i in xrange(0,N_files):
+    fos = open(files[i], 'r')
+    t,n,x,y,z = np.loadtxt(fos, delimiter=',', unpack='True')
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x,y,z,c=colors[0:len(x)], lw=0)
+    ax.scatter(x[0:3],y[0:3],z[0:3],c=colors[0:3],lw=0, s=30)
+    ax.set_title('t='+str(t[0])+', t$_{error}$ = 201.826486')
+    plt.savefig(file_names+''+str(i)+'.pdf')
+    print 'completed iteration',i
